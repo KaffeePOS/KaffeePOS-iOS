b1028de9af59db2f157930acb607799649aa425e
Marvin Birnbach <marvin@fam-birnbach.de>
2023-04-09 12:33:18 +0200

    initial commit

---
+++ KaffeePOS-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KaffeePOS-iOS
+//
+//  Created by Marvin Birnbach on 09.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
